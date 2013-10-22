83939e03c20dab3e6570cc5638e5c851592f03e8
alex520biao <baidu@baidutekiMacBook-Pro.local>
2013-10-22 17:22:39 +0800

    Initial Commit

---
+++ VIewMaskDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VIewMaskDemo
+//
+//  Created by alex on 13-10-22.
+//  Copyright (c) 2013年 alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
